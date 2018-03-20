a9c406f1e67e6c8d3d65ebdbf4fa1b3d661a158f
DavidBMcNeill <dbmcneill@github>
2018-03-20 14:53:18 -0600

    updated comments

--- BTree/src/GeneBankCreateBTree.java
+++ BTree/src/GeneBankCreateBTree.java
@@ -1,4 +1,17 @@
 
+/**
+ * 
+ * required arguments:
+ * 
+ * java GeneBankCreateBTree <0/1(no/with Cache)> <degree> <gbk file> <sequence
+ * length> [<cache size>] [<debug level>]
+ * 
+ * @authors David McNeill, Ross McCusker, Jeffrey Moore
+ */
 public class GeneBankCreateBTree {
+	
+	// degree is the degree to be used for BTree
+	
+	// 1 <= int sequence length <= 31
 
 }
--- BTree/src/GeneBankSearch.java
+++ BTree/src/GeneBankSearch.java
@@ -1,4 +1,19 @@
 
+/**
+ * required arguments:
+ * 
+ * java GeneBankSearch <0/1 (no/with Cache)> <btree file> <query file> [<cache
+ * size>] [<debug level>]
+ * 
+ * @authors David McNeill, Ross McCusker, Jeffrey Moore
+ *
+ */
 public class GeneBankSearch {
+
+	// query file contains all DNA strings of a specific sequence length that we
+	// want to search for in specified btree file
+
+	// strings are: 1 per line; all have same length as DNA sequences in btree
+	// file; use [a, c, t, g], upper- or lower-case
 
 }
